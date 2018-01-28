8ddfad380f8ffb9f9d71b0b40c09d4511d174799
Arthur Schreiber <schreiber.arthur@googlemail.com>
2018-01-28 15:35:33 +0100

    feat: enable connection encryption by default
    BREAKING CHANGE:
    
    If no `encrypt` setting was specified before, `tedious` would fall back
    to not using any encryption by default. This changes the default to
    encrypt connections unless `encrypt` is explicitly set to `false`.
    
    This _should_ work without issues with most SQL Server setups, but might
    cause connections to fail if encryption between `tedious` and SQL Server
    can not be established.

--- src/connection.js
+++ src/connection.js
@@ -128,7 +128,7 @@ class Connection extends EventEmitter {
         enableImplicitTransactions: false,
         enableNumericRoundabort: false,
         enableQuotedIdentifier: true,
-        encrypt: false,
+        encrypt: true,
         fallbackToDefaultDb: false,
         instanceName: undefined,
         isolationLevel: ISOLATION_LEVEL.READ_COMMITTED,
